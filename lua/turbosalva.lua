1d691cbc744970607b643b42c4b315790063ffcb
SalvaToro <sftpo@protonmail.ch>
2023-05-30 19:19:15 +0200

    feat: Added language detection for python, lua, rust and js

--- lua/turbosalva.lua
+++ lua/turbosalva.lua
@@ -8,10 +8,24 @@ local function insert_wrapped_word()
 	local file = fn.fnamemodify(fn.expand("%"), ":t")
 	-- Get the line number of the current cursor position
 	local line = api.nvim_win_get_cursor(0)[1]
+	-- Determine the file type
+	local filetype = vim.bo.filetype
+	-- Create the wrapped word based on the file type
+	local wrapped_word
 
-	-- Create the wrapped word with filename, line number, and the word itself
-	local wrapped_word = string.format("console.log('[%s] LN: %s', %s)", file, line, word)
-
+	if filetype == "javascript" then
+		wrapped_word = string.format("console.log('[%s] LN: %s', %s)", file, line, word)
+	elseif filetype == "python" then
+		wrapped_word = string.format("print('[%s] LN: %s', %s)", file, line, word)
+	elseif filetype == "lua" then
+		wrapped_word = string.format("print('[%s] LN: %s', %s)", file, line, word)
+	elseif filetype == "rust" then
+		wrapped_word = string.format("dbg!(%s);", word)
+	else
+		-- Default case
+		print("Unknown file type")
+		return
+	end
 	-- Insert the wrapped word on a new line after the cursor
 	api.nvim_command("normal! o" .. wrapped_word)
 	api.nvim_feedkeys(api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
